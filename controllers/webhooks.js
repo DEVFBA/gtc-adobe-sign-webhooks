2b13ea96f22c29d5529c093796a78a57005d3fae
alexisherolv <alexisherolv@gmail.com>
2022-06-02 11:35:01 -0500

    Last changes develop

--- controllers/webhooks.js
+++ controllers/webhooks.js
@@ -43,6 +43,118 @@ async function getIntencionW1(clientId){
 
 async function escucharNotificacionesW1(request){
     try{
+        console.log(request.body);
+        var invoicePool = await getInvoicePoolByASAgreement(request.body.agreement.id);
+
+        if(invoicePool!==null)
+        {
+            console.log(invoicePool)
+            var idInvoicePool = invoicePool.Id_Invoice_Pool
+            console.log(invoicePool)
+
+            const catRegister = {
+                piIdInvoicePool: idInvoicePool,
+                pvUser: "Adobe Sign",
+                pvIP: "0.0.0.0",
+                petitionType: "BACK"
+            };
+
+            //console.log(catRegister)
+
+            var accept = await acceptInvoicePool(catRegister);
+            //console.log(accept)
+
+            const generalPath                   = await getGeneralFilesPath();
+            const poolFileType                  = await getPoolCoverFileType();
+            const filePath                      = await getWorkflowFileTypePath(poolFileType);
+            var coverPath       = path.join(generalPath, filePath);
+            
+            var coverFullPath   = path.join(coverPath,  invoicePool.Header_PH_Path);
+
+            if( request.body.agreement.signedDocumentInfo.document.length > 0 ) {
+
+                try {
+    
+                    fs.writeFileSync(coverFullPath, request.body.agreement.signedDocumentInfo.document, 'base64');
+                    
+                } catch (error) {
+    
+                    console.log('Error al guardar Archivo PDF FIRMADO de la Carátula: ', error);
+                    logger.error('Error al guardar Archivo PDF FIRMADO de la Carátula: ', error);
+                    
+                }
+    
+            }
+            
+            //Por último actualizamos el Next Signer a NULO
+            const updateNextSigner = await updateInvoicePoolNextSigner(invoicePool.Id_Invoice_Pool, "-", "SIGNED");
+            if(updateNextSigner === true)
+            {
+                console.log("EL NEXT SIGNER SE ACTUALIZO A NULO")
+            }
+            else {
+                console.log("EL NEXT SIGNER NO SE ACTUALIZO A NULLO")
+            }
+        }
+    }catch(error){
+        console.log(error)
+    }
+}
+
+async function getIntencionW2(clientId){
+    try{
+        const adobeSignClientId = await getAdobeSignClientId();
+        console.log(adobeSignClientId)
+        //Validate it
+        if (clientId === adobeSignClientId) //Replace 'BGBQIIE7H253K6' with the client id of the application using which the webhook is created
+        {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }catch(error){
+        console.log(error)
+    }
+}
+
+async function escucharNotificacionesW2(request){
+    try{
+        const participantId = request.body.participantUserEmail;
+        const agreementId = request.body.agreement.id;
+        console.log(participantId)
+        console.log(agreementId)
+
+        const nextSigner = await getNextPoolASSigner(agreementId, participantId);
+        console.log(nextSigner)
+        if(nextSigner !== null)
+        {
+            const updateNextSigner = await updateInvoicePoolNextSigner(nextSigner.idInvoicePool, nextSigner.nextSigner, "IN_PROCESS");
+        }
+    }catch(error){
+        console.log(error)
+    }
+}
+
+async function getIntencionW3(clientId){
+    try{
+        const adobeSignClientId = await getAdobeSignClientId();
+        console.log(adobeSignClientId)
+        //Validate it
+        if (clientId === adobeSignClientId) //Replace 'BGBQIIE7H253K6' with the client id of the application using which the webhook is created
+        {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }catch(error){
+        console.log(error)
+    }
+}
+
+async function escucharNotificacionesW3(request){
+    try{
         const participantId = request.body.participantUserEmail;
         const agreementId = request.body.agreement.id;
         console.log(participantId)
@@ -61,5 +173,9 @@ async function escucharNotificacionesW1(request){
 
 module.exports = {
     getIntencionW1 : getIntencionW1,
-    escucharNotificacionesW1 : escucharNotificacionesW1
+    escucharNotificacionesW1 : escucharNotificacionesW1,
+    getIntencionW2 : getIntencionW2,
+    escucharNotificacionesW2 : escucharNotificacionesW2,
+    getIntencionW3 : getIntencionW3,
+    escucharNotificacionesW3 : escucharNotificacionesW3
 }
